9c8bfdcc1ac991e7ff54b3c108d152641046b3ef
Andrea <andry.tino@gmail.com>
2022-04-07 18:00:40 +0200

    Important test

--- pysrv/pysrv/tests/test_session.py
+++ pysrv/pysrv/tests/test_session.py
@@ -8,6 +8,13 @@ class MyClass:
         pass
 a = 0"""
 
+SRC_002 = """
+class MyClass:
+    def __init__(self):
+        pass
+a = 0
+a"""
+
 
 class EvaluateChunkTestCase(unittest.TestCase):
 
@@ -21,3 +28,24 @@ class EvaluateChunkTestCase(unittest.TestCase):
         # Assert
         self.assertTrue("a" in glbl, "Code not correctly executed")
         self.assertEqual(glbl["a"], 0, "Code not correctly executed")
+
+    def test_when_no_expr_ending_code_then_no_ret_value(self):
+        # Arrange
+        glbl = {}
+
+        # Act
+        result = evaluate_chunk(SRC_001, glbl)
+
+        # Assert
+        self.assertIsNone(result, "Expected no return value as chunk has no ending expr")
+
+    def test_when_expr_ending_code_then_ret_value(self):
+        # Arrange
+        glbl = {}
+
+        # Act
+        result = evaluate_chunk(SRC_002, glbl)
+
+        # Assert
+        self.assertIsNotNone(result, "Expected return value as chunk has ending expr")
+        self.assertEqual(result, 0, "Wrong returned expression result")
