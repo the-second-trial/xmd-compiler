4d63fc01c4fa44c47b06aad775f7b52959c52860
Andrea <andry.tino@gmail.com>
2022-04-11 22:29:47 +0200

    Fix heading

--- src/template_html_tufte.ts
+++ src/template_html_tufte.ts
@@ -14,7 +14,11 @@ export class HtmlTufteTemplate implements Template {
     /** @inheritdoc */
     public writeHeading(text: string, level: number): string {
         const levels = ["h1", "h2", "h3", "h4", "h5", "h6"];
-        const tagname = levels[Math.min(level + 1, levels.length)];
+        if (level <= 0 || level > levels.length) {
+            throw new Error(`Invalid level '${level}'. Allowed is 1..${levels.length}`);
+        }
+
+        const tagname = levels[Math.min(level - 1, levels.length)];
         return [
             `<${tagname}>`,
             text,
